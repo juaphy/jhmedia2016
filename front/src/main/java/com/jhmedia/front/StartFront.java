b12e52d7fb8153dd880a5ef88ef3029064071373
yhyuan <yhyuan@shendu.com>
2016-08-09 17:08:24 +0800

    commit

---
+++ front/src/main/java/com/jhmedia/front/StartFront.java
@@ -0,0 +1,26 @@
+/* =============================================================
+ * 工程名称：信息管理系统
+ * 对象名称：StartFront.java
+ * 创建时间：2016年8月9日 上午9:37:32
+ * (R) Copyright jhmedia 婧涵影视
+ * (作于：广州市番禺区大石镇植村村东南大街5号)
+ * =============================================================
+ */
+package com.jhmedia.front;
+
+/**
+ * 启动Front
+ * <pre>
+ * 概要: 通过jetty启动Front服务
+ * 更新: 2016年8月9日 上午9:37:32
+ * 作者: seki
+ * </pre>
+ */
+public class StartFront {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
